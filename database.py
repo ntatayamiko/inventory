8a595de42cbda1632dd3f7334024935aeedd5fca
ntatayamiko <ntatayamiko@gmail.com>
2025-08-22 22:16:51 -0600

    create database, add user, validate user in class UserDatabase

---
+++ database.py
@@ -0,0 +1,29 @@
+import sqlite3
+import hashlib
+
+class UserDatabase :
+    def __init__(self):
+        self.conn = sqlite3.connect('users.db')
+        self.cursor = self.conn.cursor()
+        self.create_table()
+
+    def create_table(self):
+        self.execute(''' CREATE TABLE IF NOT EXIST users ( id INTEGER PRIMARY KEY AUTOINCREMENT, username UNIQUE NOT NULL, password TEXT NOT NULL, email TEXT ) ''')
+        self.conn.commit()
+
+    def add_user(self, username, password, email=None):
+        hashed_password = hashlib.sha256(password.encode()).hexdigest()
+        try:
+            self.cursor.execute('''INSERT INTO users (username, password,email) VALUES (?,?,?) ''',(username, hashed_password, email))
+            self.conn.commit()
+            return True
+        except sqlite3.IntegrityError:
+            return False #username already exist
+                        
+    def validate_user (self, username, password ):
+        hashed_password = hashlib.sha256(password.encode()).hexdigest()
+        self.cursor.execute(''' SELECT FROM users WHERE username =? AND password = ? ''', (username, hashed_password))
+        return cursor.fetchone() is not None
+
+    def close(self):
+        self.conn.close()
--- gui.py
+++ gui.py
@@ -45,47 +45,47 @@ class Login ( wx.Panel ):
 		
 		gSizer1 = wx.GridSizer( 0, 2, 0, 0 )
 		
-		self.Username = wx.StaticText( self, wx.ID_ANY, u"USERNAME:", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.Username.Wrap( -1 )
-		gSizer1.Add( self.Username, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL|wx.ALIGN_BOTTOM, 5 )
+		self.username = wx.StaticText( self, wx.ID_ANY, u"USERNAME:", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.username.Wrap( -1 )
+		gSizer1.Add( self.username, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL|wx.ALIGN_BOTTOM, 5 )
 		
-		self.m_textCtrl3 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
-		gSizer1.Add( self.m_textCtrl3, 0, wx.ALL|wx.ALIGN_BOTTOM, 5 )
+		self.name_textCtrl3 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+		gSizer1.Add( self.name_textCtrl3, 0, wx.ALL|wx.ALIGN_BOTTOM, 5 )
 		
-		self.Password = wx.StaticText( self, wx.ID_ANY, u"PASSWORD:", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.Password.Wrap( -1 )
-		gSizer1.Add( self.Password, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL|wx.ALIGN_CENTER_VERTICAL, 5 )
+		self.password = wx.StaticText( self, wx.ID_ANY, u"PASSWORD:", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.password.Wrap( -1 )
+		gSizer1.Add( self.password, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL|wx.ALIGN_CENTER_VERTICAL, 5 )
 		
-		self.m_textCtrl4 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
-		gSizer1.Add( self.m_textCtrl4, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5 )
+		self.password_textCtrl4 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+		gSizer1.Add( self.password_textCtrl4, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5 )
 		
-		self.Email = wx.StaticText( self, wx.ID_ANY, u"EMAIL:", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.Email.Wrap( -1 )
-		gSizer1.Add( self.Email, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
+		self.email = wx.StaticText( self, wx.ID_ANY, u"EMAIL:", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.email.Wrap( -1 )
+		gSizer1.Add( self.email, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
 		
-		self.m_textCtrl5 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
-		gSizer1.Add( self.m_textCtrl5, 0, wx.ALL, 5 )
+		self.email_textCtrl5 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+		gSizer1.Add( self.email_textCtrl5, 0, wx.ALL, 5 )
 		
 		
 		bSizer1.Add( gSizer1, 1, wx.EXPAND, 5 )
 		
-		self.Login = wx.Button( self, wx.ID_ANY, u"LOGIN", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.Login.SetBackgroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_INACTIVECAPTION ) )
+		self.login = wx.Button( self, wx.ID_ANY, u"LOGIN", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.login.SetBackgroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_INACTIVECAPTION ) )
 		
-		bSizer1.Add( self.Login, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
+		bSizer1.Add( self.login, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
 		
-		self.SignIn = wx.Button( self, wx.ID_ANY, u"SIGN IN", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.SignIn.SetBackgroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_INACTIVECAPTION ) )
+		self.signIn = wx.Button( self, wx.ID_ANY, u"SIGN IN", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.signIn.SetBackgroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_INACTIVECAPTION ) )
 		
-		bSizer1.Add( self.SignIn, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
+		bSizer1.Add( self.signIn, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
 		
 		
 		self.SetSizer( bSizer1 )
 		self.Layout()
 		
 		# Connect Events
-		self.Login.Bind( wx.EVT_BUTTON, self.login )
-		self.SignIn.Bind( wx.EVT_BUTTON, self.sign_in_panel )
+		self.login.Bind( wx.EVT_BUTTON, self.login )
+		self.signIn.Bind( wx.EVT_BUTTON, self.sign_in_panel )
 	
 	def __del__( self ):
 		pass
@@ -115,41 +115,41 @@ class SignInPanel ( wx.Panel ):
 		
 		gSizer2 = wx.GridSizer( 0, 2, 0, 0 )
 		
-		self.Username = wx.StaticText( self, wx.ID_ANY, u"USERNAME :", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.Username.Wrap( -1 )
-		gSizer2.Add( self.Username, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL|wx.ALIGN_BOTTOM, 5 )
+		self.username = wx.StaticText( self, wx.ID_ANY, u"USERNAME :", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.username.Wrap( -1 )
+		gSizer2.Add( self.username, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL|wx.ALIGN_BOTTOM, 5 )
 		
-		self.m_textCtrl6 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
-		gSizer2.Add( self.m_textCtrl6, 0, wx.ALL|wx.ALIGN_BOTTOM, 5 )
+		self.username_textCtrl6 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+		gSizer2.Add( self.username_textCtrl6, 0, wx.ALL|wx.ALIGN_BOTTOM, 5 )
 		
-		self.Password = wx.StaticText( self, wx.ID_ANY, u"PASSWORD :", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.Password.Wrap( -1 )
-		gSizer2.Add( self.Password, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL|wx.ALIGN_CENTER_VERTICAL, 5 )
+		self.password = wx.StaticText( self, wx.ID_ANY, u"PASSWORD :", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.password.Wrap( -1 )
+		gSizer2.Add( self.password, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL|wx.ALIGN_CENTER_VERTICAL, 5 )
 		
-		self.m_textCtrl7 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
-		gSizer2.Add( self.m_textCtrl7, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5 )
+		self.password_textCtrl7 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+		gSizer2.Add( self.password_textCtrl7, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5 )
 		
-		self.Email = wx.StaticText( self, wx.ID_ANY, u"EMAIL :", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.Email.Wrap( -1 )
-		gSizer2.Add( self.Email, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
+		self.email = wx.StaticText( self, wx.ID_ANY, u"EMAIL :", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.email.Wrap( -1 )
+		gSizer2.Add( self.email, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
 		
-		self.m_textCtrl8 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
-		gSizer2.Add( self.m_textCtrl8, 0, wx.ALL, 5 )
+		self.email_textCtrl8 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+		gSizer2.Add( self.email_textCtrl8, 0, wx.ALL, 5 )
 		
 		
 		bSizer2.Add( gSizer2, 1, wx.EXPAND, 5 )
 		
-		self.SigninPanelButt = wx.Button( self, wx.ID_ANY, u"Sign-in", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.SigninPanelButt.SetBackgroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_ACTIVECAPTION ) )
+		self.sign_in_panel = wx.Button( self, wx.ID_ANY, u"Sign-in", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.sign_in_panel.SetBackgroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_ACTIVECAPTION ) )
 		
-		bSizer2.Add( self.SigninPanelButt, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
+		bSizer2.Add( self.sign_in_panel, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
 		
 		
 		self.SetSizer( bSizer2 )
 		self.Layout()
 		
 		# Connect Events
-		self.SigninPanelButt.Bind( wx.EVT_BUTTON, self.sign_in )
+		self.sign_in_panel.Bind( wx.EVT_BUTTON, self.sign_in )
 	
 	def __del__( self ):
 		pass
